2f485c37c004b0a62286b19257713b7cdf06fdfe
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2023-08-21 21:13:58 +0100

    Created User model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class User extends Equatable {
+  final int id;
+  final String name;
+
+  const User(this.id, this.name);
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+
+  @override
+  List<Object?> get props => [id, name];
+}
