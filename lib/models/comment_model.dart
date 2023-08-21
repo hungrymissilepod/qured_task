40ff07edccf82a1ba60d4240f82b9592d6b5fb7e
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2023-08-21 22:02:20 +0100

    Added Comment model

---
+++ lib/models/comment_model.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+
+part 'comment_model.g.dart';
+
+@JsonSerializable()
+class Comment extends Equatable {
+  final int postId;
+  final int id;
+  final String name;
+  final String email;
+  final String body;
+
+  const Comment(this.postId, this.id, this.name, this.email, this.body);
+
+  factory Comment.fromJson(Map<String, dynamic> json) => _$CommentFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CommentToJson(this);
+
+  @override
+  List<Object?> get props => [postId, id, name, email, body];
+}
---
+++ lib/models/comment_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'comment_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Comment _$CommentFromJson(Map<String, dynamic> json) => Comment(
+      json['postId'] as int,
+      json['id'] as int,
+      json['name'] as String,
+      json['email'] as String,
+      json['body'] as String,
+    );
+
+Map<String, dynamic> _$CommentToJson(Comment instance) => <String, dynamic>{
+      'postId': instance.postId,
+      'id': instance.id,
+      'name': instance.name,
+      'email': instance.email,
+      'body': instance.body,
+    };
